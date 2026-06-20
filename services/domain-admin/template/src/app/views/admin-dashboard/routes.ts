5d03726be1184699664fe9bf49f7915e99227d7c
NunezLagos <asistente.nunez@gmail.com>
2026-06-19 20:56:11 -0400

    chore: sunset domain-admin angular y domain-frontend legacy

--- services/domain-admin/template/src/app/views/admin-dashboard/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { AdminDashboardComponent } from './dashboard.component';
-
-export const routes: Routes = [
-  {
-    path: '',
-    component: AdminDashboardComponent,
-  },
-];
--- services/domain-admin/template/src/app/views/admin-members/routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { AdminMembersComponent } from './members.component';
-
-export const routes: Routes = [
-  { path: '', component: AdminMembersComponent },
-];
